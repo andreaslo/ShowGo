067d9e14b6688bc2a63fb65f7546a8cc908cbf44
andreas <andreas>
2013-04-20 19:37:11 +0200

    Confirmation dialog added

--- src/de/feu/showgo/ui/actions/NewAction.java
+++ src/de/feu/showgo/ui/actions/NewAction.java
@@ -3,28 +3,33 @@ package de.feu.showgo.ui.actions;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import de.feu.showgo.ShowGoDAO;
 import de.feu.showgo.model.ShowGo;
 import de.feu.showgo.ui.MainWindow;
 
 public class NewAction implements ActionListener {
-	
+
 	private MainWindow mainWindow;
 
 	public NewAction(MainWindow mainWindow) {
 		this.mainWindow = mainWindow;
-	 }
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		ShowGoDAO.setShowGo(new ShowGo());
-		ShowGoDAO.setSaveFile(null);
-		mainWindow.setTitleFilename(null);
-		mainWindow.getNavTree().refreshTree();
-		mainWindow.showStartupView();
 	}
 
-	
-	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int result = JOptionPane.showConfirmDialog(mainWindow,
+				"Sind Sie sicher, dass Sie ein neues Theater anlegen wollen? Alle nicht gespeicherten Daten gehen verloren.", "Neues Theater",
+				JOptionPane.YES_NO_OPTION);
+		if (result == JOptionPane.YES_OPTION) {
+			ShowGoDAO.setShowGo(new ShowGo());
+			ShowGoDAO.setSaveFile(null);
+			mainWindow.setTitleFilename(null);
+			mainWindow.getNavTree().refreshTree();
+			mainWindow.showStartupView();
+		}
+
+	}
+
 }
