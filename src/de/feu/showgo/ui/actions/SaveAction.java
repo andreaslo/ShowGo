fe6a1dc75a06ba9ee8b10825e9e898c72e19be03
andreas <andreas>
2013-04-20 12:09:31 +0200

    If a save file exists, the user is not asked to select a file.

--- src/de/feu/showgo/ui/actions/SaveAction.java
+++ src/de/feu/showgo/ui/actions/SaveAction.java
@@ -3,12 +3,8 @@ package de.feu.showgo.ui.actions;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
 
-import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.bind.JAXBException;
 
 import org.apache.log4j.Logger;
@@ -28,19 +24,33 @@ public class SaveAction implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		ShowGoFileChooser fileChooser = new ShowGoFileChooser(mainWindow);
-		fileChooser.showDialog();
-		if (fileChooser.isApproved()) {
-			File file = fileChooser.getSelectedFile();
-			try {
-				ShowGoDAO.saveToDisc(file);
-			} catch (JAXBException e) {
-				log.error("",e);
-				e.printStackTrace();
-				JOptionPane.showMessageDialog(mainWindow, "Es ist ein Fehler beim Speichern aufgetreten: " + e.getMessage(), "Fehler beim Speichern",
-						JOptionPane.ERROR_MESSAGE);
+		// If a save file exists, overwrite it
+		File target = ShowGoDAO.getSaveFile();
+		
+		if(ShowGoDAO.getSaveFile() == null){
+			// Ask user to select a file
+			ShowGoFileChooser fileChooser = new ShowGoFileChooser(mainWindow);
+			fileChooser.showDialog();
+			if (fileChooser.isApproved()) {
+				target = fileChooser.getSelectedFile();
+			}else{
+				log.debug("Aborted by user");
+				return;
 			}
-			ShowGoDAO.setSaveFile(file);
 		}
+
+		saveShowGo(target);
+	}
+	
+	private void saveShowGo(File file){
+		try {
+			ShowGoDAO.saveToDisc(file);
+		} catch (JAXBException e) {
+			log.error("",e);
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(mainWindow, "Es ist ein Fehler beim Speichern aufgetreten: " + e.getMessage(), "Fehler beim Speichern",
+					JOptionPane.ERROR_MESSAGE);
+		}
+		ShowGoDAO.setSaveFile(file);
 	}
 }
