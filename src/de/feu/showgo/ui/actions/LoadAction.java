d36d8706920d266c4e6950a2eeadd18267039638
andreas <andreas>
2013-04-20 19:44:54 +0200

    Load action created

---
+++ src/de/feu/showgo/ui/actions/LoadAction.java
@@ -0,0 +1,21 @@
+package de.feu.showgo.ui.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import de.feu.showgo.ui.MainWindow;
+
+public class LoadAction implements ActionListener {
+
+	private MainWindow mainWindow;
+
+	public LoadAction(MainWindow mainWindow) {
+		this.mainWindow = mainWindow;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
