cc221d0cace60b37b880d4f5fd8f2d6b58c4fc5a
andreas <andreas>
2013-04-21 12:57:27 +0200

    log4j BasicConfigurator added

--- src/de/feu/showgo/Main.java
+++ src/de/feu/showgo/Main.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import javax.swing.UIManager;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
@@ -18,6 +19,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 
+		BasicConfigurator.configure();
+
+		
 		PlasticLookAndFeel.setPlasticTheme(new DesertBlue());
 		try {
 			UIManager.setLookAndFeel(new Plastic3DLookAndFeel());
