e57e677d4aa78e34156d27451d48d492c521583b
andreas <andreas>
2013-04-29 19:20:01 +0200

    added test case for saving and loading showgo objects containing plays

--- test/de/feu/showgo/io/ShowGoIOTest.java
+++ test/de/feu/showgo/io/ShowGoIOTest.java
@@ -1,6 +1,6 @@
 package de.feu.showgo.io;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +14,7 @@ import org.junit.Test;
 import de.feu.showgo.model.Gender;
 import de.feu.showgo.model.Person;
 import de.feu.showgo.model.ShowGo;
+import de.feu.showgo.model.TheaterPlay;
 
 public class ShowGoIOTest {
 
@@ -40,8 +41,49 @@ public class ShowGoIOTest {
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	@Test
+	public void assignedPlaysLoadStore() {
+		ShowGo saveObj = new ShowGo();
+		Person p1 = new Person();
+		p1.setName("TestName");
+		p1.setGender(Gender.FEMALE);
+		p1.setBirthday(new Date());
+		p1.setWordsRetention(1234);
+		saveObj.addPerson(p1);
 		
+		PlayParser parser = new PlayParser();
+		File macBethInput = new File("testData" + System.getProperty("file.separator") + "Macbeth.html");
+		File macShortInput = new File("testData" + System.getProperty("file.separator") + "Macshort.html");
+		TheaterPlay macBeth = null;
+		TheaterPlay macShort = null;
+		try {
+			macBeth = parser.generatePlay(macBethInput);
+			macShort = parser.generatePlay(macShortInput);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (ParsingException e1) {
+			e1.printStackTrace();
+		}
 		
+		assertNotNull(macBeth);
+		assertNotNull(macShort);
+		saveObj.addPlay(macShort);
+		saveObj.addPlay(macBeth);
+		
+		try {
+			File tempFile = File.createTempFile("showgo", "xml");
+			ShowGoIO.saveShowGo(saveObj, tempFile);
+			ShowGo loadObj = ShowGoIO.loadShowGo(tempFile);
+			assertEquals(1, loadObj.getPersons().size());
+			assertEquals(saveObj, loadObj);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
