93c86064727e2ac157aa04327a495bfff8938e39
andreas <andreas>
2013-04-30 12:06:36 +0200

    Ensemble class added

---
+++ src/de/feu/showgo/model/Ensemble.java
@@ -0,0 +1,11 @@
+package de.feu.showgo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ensemble {
+
+	private String name;
+	private List<Person> members = new ArrayList<Person>();
+	
+}
