843feaf2813dda82f3f54a629e62f87f53f2216a
andreas <andreas>
2013-04-30 15:12:19 +0200

    Ensemble marshalling test case added

--- test/de/feu/showgo/io/ShowGoIOTest.java
+++ test/de/feu/showgo/io/ShowGoIOTest.java
@@ -11,6 +11,7 @@ import javax.xml.bind.JAXBException;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import de.feu.showgo.model.Ensemble;
 import de.feu.showgo.model.Gender;
 import de.feu.showgo.model.Person;
 import de.feu.showgo.model.ShowGo;
@@ -45,7 +46,51 @@ public class ShowGoIOTest {
 	
 	
 	@Test
-	public void assignedPlaysLoadStore() {
+	public void ensembleLoadStore() {
+		ShowGo saveObj = new ShowGo();
+		Person p1 = new Person();
+		p1.setName("TestName");
+		p1.setGender(Gender.FEMALE);
+		p1.setBirthday(new Date());
+		p1.setWordsRetention(1234);
+		saveObj.addPerson(p1);
+		
+		Person p2 = new Person();
+		p2.setName("\"\"\"");
+		p2.setGender(Gender.MALE);
+		p2.setBirthday(new Date());
+		p2.setWordsRetention(1234);
+		saveObj.addPerson(p2);
+		
+		Person p3 = new Person();
+		p3.setName("<id>blibla</id>");
+		p3.setGender(Gender.FEMALE);
+		p3.setBirthday(new Date());
+		p3.setWordsRetention(4433);
+		saveObj.addPerson(p3);
+		
+		Ensemble ensemble = new Ensemble();
+		ensemble.assignPerson(p1);
+		ensemble.assignPerson(p2);
+		ensemble.assignPerson(p3);
+		
+		
+		try {
+			File tempFile = File.createTempFile("showgo", "xml");
+			ShowGoIO.saveShowGo(saveObj, tempFile);
+			ShowGo loadObj = ShowGoIO.loadShowGo(tempFile);
+			assertEquals(3, loadObj.getPersons().size());
+			assertEquals(1, loadObj.getEnsembles().size());
+			assertEquals(saveObj, loadObj);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void assignedPlaysLoadStoreWithEnsemble() {
 		ShowGo saveObj = new ShowGo();
 		Person p1 = new Person();
 		p1.setName("TestName");
