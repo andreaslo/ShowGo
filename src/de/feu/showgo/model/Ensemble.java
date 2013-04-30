7ca3cdfff40e5bfa2560a532dcd89edd67b028ba
andreas <andreas>
2013-04-30 15:15:35 +0200

    JAXB annotations, equals and hashCode methods added

--- src/de/feu/showgo/model/Ensemble.java
+++ src/de/feu/showgo/model/Ensemble.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlIDREF;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Ensemble {
 
+	@XmlElement(name="name")
 	private String name;
+	@XmlIDREF
 	private List<Person> members = new ArrayList<Person>();
+	
 	public String getName() {
 		return name;
 	}
@@ -25,5 +34,36 @@ public class Ensemble {
 	public void removePerson(Person person){
 		members.remove(person);
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((members == null) ? 0 : members.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ensemble other = (Ensemble) obj;
+		if (members == null) {
+			if (other.members != null)
+				return false;
+		} else if (!members.equals(other.members))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
