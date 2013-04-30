d7d2a6386b9c98574ca5144fc5a96998040c53ac
andreas <andreas>
2013-04-30 14:17:53 +0200

    Getter and setter added

--- src/de/feu/showgo/model/Ensemble.java
+++ src/de/feu/showgo/model/Ensemble.java
@@ -1,11 +1,25 @@
 package de.feu.showgo.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Ensemble {
 
 	private String name;
 	private List<Person> members = new ArrayList<Person>();
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Person> getMembers() {
+		return Collections.unmodifiableList(members);
+	}
+	
+	
+	
+	
 	
 }
