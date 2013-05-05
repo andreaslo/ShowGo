90235438012ae92d01138c7378c6805066218289
andreas <andreas>
2013-05-05 16:52:51 +0200

    Added field for non-actor roles

--- src/de/feu/showgo/model/Production.java
+++ src/de/feu/showgo/model/Production.java
@@ -1,8 +1,11 @@
 package de.feu.showgo.model;
 
+import java.util.List;
+
 public class Production {
 
 	private TheaterPlay play;
+	private List<Role> nonActorRoles;
 
 	public TheaterPlay getPlay() {
 		return play;
@@ -11,5 +14,15 @@ public class Production {
 	public void setPlay(TheaterPlay play) {
 		this.play = play;
 	}
+
+	public List<Role> getNonActorRoles() {
+		return nonActorRoles;
+	}
+
+	public void setNonActorRoles(List<Role> nonActorRoles) {
+		this.nonActorRoles = nonActorRoles;
+	}
+	
+	
 	
 }
