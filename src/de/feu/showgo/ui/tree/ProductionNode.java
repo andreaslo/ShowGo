fb11ab91c25416be4b0f46de835711329c165bbb
andreas <andreas>
2013-05-05 17:50:12 +0200

    ProdoctionNode created

---
+++ src/de/feu/showgo/ui/tree/ProductionNode.java
@@ -0,0 +1,45 @@
+package de.feu.showgo.ui.tree;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+import de.feu.showgo.model.Production;
+import de.feu.showgo.ui.MainWindow;
+
+public class ProductionNode extends TreeElement{
+
+		private MainWindow mainWindow;
+		private Production production;
+		
+		public ProductionNode(Production production, MainWindow mainWindow) {
+			super(production);
+			this.production = production;
+			this.mainWindow = mainWindow;
+		}
+
+		private static final long serialVersionUID = 1L;
+		
+		@Override
+		public JPopupMenu getPopupMenu() {
+			JPopupMenu popup = new JPopupMenu();
+			
+			JMenuItem showEnsemble = new JMenuItem("Anzeigen");
+			
+			JMenuItem editEnsemble = new JMenuItem("Bearbeiten");
+			
+			JMenuItem deleteEnsemble = new JMenuItem("Löschen");
+			
+			popup.add(showEnsemble);
+			popup.add(editEnsemble);
+			popup.addSeparator();
+			popup.add(deleteEnsemble);
+			return popup;
+		}
+
+		@Override
+		public String toString() {
+			return production.getName();
+		}
+		
+		
+}
