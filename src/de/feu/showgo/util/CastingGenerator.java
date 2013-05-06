bd954c73229aee072872bfb1ce613c6257b9c3d6
andreas <andreas>
2013-05-06 21:43:06 +0200

    generateCasting method and class created

---
+++ src/de/feu/showgo/util/CastingGenerator.java
@@ -0,0 +1,19 @@
+package de.feu.showgo.util;
+
+import de.feu.showgo.model.Ensemble;
+import de.feu.showgo.model.TheaterPlay;
+
+public class CastingGenerator {
+
+	
+	public static void generateCasting(Ensemble ensemble, TheaterPlay play){
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
