4bb089cc8e34b6111462d32a540e27bf1b84f79f
andreas <andreas>
2013-05-08 13:41:46 +0200

    Ensembels assigned to a production can now longer be edited

--- src/de/feu/showgo/ui/actions/EditEnsembleAction.java
+++ src/de/feu/showgo/ui/actions/EditEnsembleAction.java
@@ -3,7 +3,11 @@ package de.feu.showgo.ui.actions;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
+import de.feu.showgo.ShowGoDAO;
 import de.feu.showgo.model.Ensemble;
+import de.feu.showgo.model.Production;
 import de.feu.showgo.ui.MainWindow;
 
 public class EditEnsembleAction implements ActionListener {
@@ -19,6 +23,14 @@ public class EditEnsembleAction implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		for(Production production : ShowGoDAO.getShowGo().getProductions()){
+			if(production.getEnsamble() == ensemble){
+				JOptionPane.showMessageDialog(mainWindow, "Das Ensemble " + ensemble.getName() + " ist der Inszenierung " + production.getName()
+						+ " zugeordnet und kann daher nicht bearbeitet werden.", "Ensemble Inszenierung zugeordnet", JOptionPane.INFORMATION_MESSAGE);
+				return;
+			}
+		}
+		
 		mainWindow.showEditEnsemble(ensemble, true);
 	}
 
