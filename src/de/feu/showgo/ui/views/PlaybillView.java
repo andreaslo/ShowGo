387aabfc406043055209a4ae77becf8960c796a9
andreas <andreas>
2013-05-09 10:56:01 +0200

    The name of the play, number of acts and casting of the actors is now displayed

--- src/de/feu/showgo/ui/views/PlaybillView.java
+++ src/de/feu/showgo/ui/views/PlaybillView.java
@@ -10,6 +10,7 @@ import javax.swing.JTextArea;
 import org.apache.log4j.Logger;
 
 import de.feu.showgo.model.Production;
+import de.feu.showgo.model.Role;
 import de.feu.showgo.ui.MainWindow;
 
 public class PlaybillView extends JPanel {
@@ -36,6 +37,7 @@ public class PlaybillView extends JPanel {
 		
 		JTextArea playbillArea = new JTextArea();
 		playbillArea.setBorder(BorderFactory.createEtchedBorder());
+		playbillArea.setText(generatePlaybillText(production));
 		
 		add(saveButton,"1,1,l,c");
 		add(playbillArea, "1,3");
@@ -43,5 +45,53 @@ public class PlaybillView extends JPanel {
 		revalidate();
 		repaint();
 	}
+	
+	private String generatePlaybillText(Production prod){
+		StringBuilder playbill = new StringBuilder();
+		
+		playbill.append(prod.getPlay().getName());
+		playbill.append("\n");
+		playbill.append("Ein Stück in ");
+		playbill.append(prod.getPlay().getActs().size());
+		playbill.append(" Akten");
+		playbill.append("\n");
+		playbill.append("\n");
+		playbill.append("Besetzung der Darstellerrollen:");
+		playbill.append("\n");
+		playbill.append("\n");
+		
+		for(Role role : prod.getPlay().getRoles()){
+			if(role.isPseudoRole()){
+				continue;
+			}
+			playbill.append(role.getName());
+			playbill.append(": ");
+			if(role.getCast().size() == 1){
+				playbill.append(role.getCast().get(0).getName());
+			}else if(role.getCast().size() == 2){
+				playbill.append(role.getCast().get(0).getName());
+				playbill.append(" und ");
+				playbill.append(role.getCast().get(1).getName());
+				playbill.append(" als zweite Besetzung");
+			}else{
+				playbill.append(role.getCast().get(0).getName());
+				playbill.append("\nWeitere Besetzugen: ");
+				for(int i = 1; i < role.getCast().size(); i++){
+					playbill.append(role.getCast().get(i).getName());
+					if(i == role.getCast().size() - 2){
+						playbill.append(" und ");
+					}else{
+						playbill.append(", ");
+					}
+				}
+			}
+			
+			playbill.append("\n");
+			playbill.append("\n");
+		}
+		
+		return playbill.toString();
+	}
+	
 	
 }
