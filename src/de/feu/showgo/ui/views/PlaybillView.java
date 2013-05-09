302a83d77f664ac227c4f3d90f4323e7d3dee5d8
andreas <andreas>
2013-05-09 10:28:02 +0200

    Title added

--- src/de/feu/showgo/ui/views/PlaybillView.java
+++ src/de/feu/showgo/ui/views/PlaybillView.java
@@ -1,5 +1,7 @@
 package de.feu.showgo.ui.views;
 
+import info.clearthought.layout.TableLayout;
+
 import javax.swing.JPanel;
 
 import org.apache.log4j.Logger;
@@ -17,11 +19,18 @@ public class PlaybillView extends JPanel {
 		log.debug("showing playbill view for " + production);
 		this.mainWindow = mainWindow;
 		this.production = production;
+		setName("Programmheft");
 		createComponent();
 	}
 	
 	private void createComponent(){
+		double size[][] = { { 20, TableLayout.FILL, 20 },
+				{ 20, 60, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, 30, 30 } };
+		setLayout(new TableLayout(size));
 		
+		
+		revalidate();
+		repaint();
 	}
 	
 }
