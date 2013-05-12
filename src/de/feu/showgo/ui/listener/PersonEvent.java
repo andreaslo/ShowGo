6b47db203df433ff85e56008b3c94ef9f5747192
andreas <andreas>
2013-05-12 18:35:20 +0200

    JavaDoc comments added and imports organized

--- src/de/feu/showgo/ui/listener/PersonEvent.java
+++ src/de/feu/showgo/ui/listener/PersonEvent.java
@@ -2,8 +2,16 @@ package de.feu.showgo.ui.listener;
 
 import de.feu.showgo.model.Person;
 
+/**
+ * A callback for changes on a person.
+ */
 public interface PersonEvent {
 
+	/**
+	 * The person that was changed.
+	 * 
+	 * @param person
+	 */
 	public void personEvent(Person person);
-	
+
 }
--- src/de/feu/showgo/ui/listener/RoleDeleteListener.java
+++ src/de/feu/showgo/ui/listener/RoleDeleteListener.java
@@ -2,8 +2,16 @@ package de.feu.showgo.ui.listener;
 
 import de.feu.showgo.model.Role;
 
+/**
+ * A callback that triggers if a role is deleted.
+ */
 public interface RoleDeleteListener {
 
+	/**
+	 * A callback that triggers if the provided role was deleted.
+	 * 
+	 * @param role
+	 */
 	public void deleteRole(Role role);
-	
+
 }
