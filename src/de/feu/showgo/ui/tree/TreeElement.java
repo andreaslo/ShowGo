74333c374c49c5f08935e6b0c575a11e118007fb
andreas <andreas>
2013-05-12 20:46:47 +0200

    Added abstract modifier to class

--- src/de/feu/showgo/ui/tree/TreeElement.java
+++ src/de/feu/showgo/ui/tree/TreeElement.java
@@ -1,13 +1,9 @@
 package de.feu.showgo.ui.tree;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.tree.DefaultMutableTreeNode;
 
-public class TreeElement extends DefaultMutableTreeNode {
+public abstract class TreeElement extends DefaultMutableTreeNode {
 	
 	private static final long serialVersionUID = 1L;
 	
