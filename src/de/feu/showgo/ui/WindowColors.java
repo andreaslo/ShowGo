eb88a621e68d54a6c4ed39e14206da84ef72b629
andreas <andreas>
2013-05-12 13:29:48 +0200

    JavaDoc comments added and imports organized

--- src/de/feu/showgo/ui/WindowColors.java
+++ src/de/feu/showgo/ui/WindowColors.java
@@ -2,9 +2,19 @@ package de.feu.showgo.ui;
 
 import java.awt.Color;
 
+/**
+ * A class containing constants for colors used in the application ui.
+ * 
+ */
 public class WindowColors {
 
-	public static final Color ERROR = new Color(0xff,0x45,0x45);
+	/**
+	 * Red color that should be used for error messages.
+	 */
+	public static final Color ERROR = new Color(0xff, 0x45, 0x45);
+	/**
+	 * Green color that should be used for success messages.
+	 */
 	public static final Color SUCCESS = Color.GREEN;
 
 }
